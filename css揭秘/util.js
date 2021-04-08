8b0f29abe3b5d8a8ca48fe48e323283108cd6e29
rocky <rocky.zhang@ximalaya.com>
2021-04-08 20:41:53 +0800

    feat: css揭秘

---
+++ css揭秘/util.js
@@ -0,0 +1,25 @@
+/**
+ * 查找元素
+ * @param {*} selector
+ * @param {*} context
+ */
+function $$(selector, context) {
+  context = context || document;
+  const elements = context.querySelectorAll(selector);
+  return Array.prototype.slice.call(elements);
+}
+
+/**
+ * 查看css属性是否存在，并添加到class上
+ * @param {*} property
+ */
+function testProperty(property) {
+  const root = document.documentElement;
+
+  if (property in root.style) {
+    root.classList.add(property.toLowerCase());
+    return true;
+  }
+
+  root.classList.add("no-" + property.toLowerCase());
+}
