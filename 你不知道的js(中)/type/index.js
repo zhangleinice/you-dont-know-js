a0ed0a9ee6a921e8aae1ad57bbaf5d9cdc1f64e5
rocky <rocky.zhang@ximalaya.com>
2021-04-15 20:49:53 +0800

    feat: type

--- 你不知道的js(中)/type/index.js
+++ 你不知道的js(中)/type/index.js
@@ -1,3 +1,26 @@
 let a = null;
 
-console.log(a === null);
+function foo(a, b) {}
+console.log(foo.length); // 2
+
+console.log(Object.getOwnPropertyNames(foo));
+
+// x is undeclared, but typeof x is undefined
+console.log(typeof x);
+
+// 这样会抛出错误
+// if (x) {
+//   console.log(x);
+// }
+
+// 这样是安全的
+if (typeof x !== "undefined") {
+  console.log(x);
+}
+
+// 这样是安全的
+if (globalThis.x) {
+  console.log(globalThis.x);
+}
+
+// 依赖注入 dependency injection  当成函数参数
